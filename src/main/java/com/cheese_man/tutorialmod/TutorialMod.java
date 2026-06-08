d97fb24c2b1caa3b62ee029e6ee7a1ce090b1a32
westin <westin.lambert2010@gmail.com>
2026-06-08 08:55:20 +0000

    we do a little silly part 2

--- src/main/java/com/cheese_man/tutorialmod/TutorialMod.java
+++ src/main/java/com/cheese_man/tutorialmod/TutorialMod.java
@@ -1,5 +1,7 @@
 package com.cheese_man.tutorialmod;
 
+import com.cheese_man.tutorialmod.block.ModBlocks;
+import com.cheese_man.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -49,6 +51,9 @@ public class TutorialMod {
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -62,8 +67,15 @@ public class TutorialMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.BISMUTH);
+            event.accept(ModItems.RAW_BISMUTH);
+        }
 
-    }
+        if(event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
+            event.accept(ModBlocks.BISMUTH_BLOCK);
+        }
+     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
